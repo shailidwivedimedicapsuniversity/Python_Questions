904c2ab342079b99d53c0bc6aef499ccc78aa0b6
Shaili <shaili.d@zecdata.com>
2024-06-05 15:27:30 +0530

    created new file

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -15,3 +15,27 @@ print (myDict)
 
 dic = dict.fromkeys(range(5), True)
 print(dic)
+
+# creation using list comprehension
+myDict = {x: x**2 for x in [1,2,3,4,5]}
+print (myDict)
+
+sDict = {x.upper(): x*3 for x in 'coding '}
+print (sDict)
+
+# comprehension using if.
+newdict = {x: x**3 for x in range(10) if x**3 % 4 == 0}
+print(newdict)
+
+# given string
+l="GFG"
+
+# using nested dictionary comprehension
+dic = {
+	x: {y: x + y for y in l} for x in l
+}
+
+print(dic) # output - {'G': {'G': 'GG', 'F': 'GF'}, 'F': {'G': 'FG', 'F': 'FF'}}
+
+# keys should be unique in dic for that this is not output
+# {'G': {'G': 'GG', 'F': 'GF', 'G':'GG'}, 'F': {'G': 'FG', 'F': 'FF', 'G':'FG'}, 'G':{'G': 'GG', 'F':'GF', 'G':'GG'}} 
